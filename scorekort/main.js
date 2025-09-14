fd42447b62f9a5f9b14baad901b660bb9b5588be
Sniffo5 <aronloreskar@gmail.com>
2025-09-14 18:07:26 +0200

    Uploaded updated version of ScoreKort (Not final)

---
+++ scorekort/main.js
@@ -0,0 +1,34 @@
+import { startHtmlGeneration } from './mod/startMenu.js';
+import { startNewGame, loadGameScore } from './mod/startGame.js';
+
+document.getElementById('newGame').addEventListener('click', function () {
+    startHtmlGeneration();
+});
+
+const loadGameBtn = document.getElementById('loadGame');
+if (loadGameBtn) {
+    loadGameBtn.onclick = function () {
+        loadGameScore();
+    };
+}
+
+async function getInfo() {
+
+    try {
+        let info = await fetch('info.json');
+        let infoData = await info.json();
+
+        return infoData;
+
+    } catch (error) {
+        console.error('Error fetching info:', error);
+    }
+
+}
+
+let infoData = await getInfo();
+let courtData = infoData.court;
+
+console.log(courtData);
+
+export { courtData };
